b567838488943f26bd4bc05bb31a418c00db2f9b
Flavian Alexandru <alexandruflavian23@gmail.com>
2015-07-12 23:18:33 +0100

    Adding basic tests

---
+++ engine/src/test/scala/com/websudos/diesel/engine/query/AbstractQueryTest.scala
@@ -0,0 +1,18 @@
+package com.websudos.diesel.engine.query
+
+import org.scalatest.{FlatSpec, Matchers}
+
+case class DummyQuery(override val queryString: String) extends AbstractQuery[DummyQuery](queryString) {
+  override def create(st: String): DummyQuery = DummyQuery(st)
+}
+
+object DummyQuery {
+  def empty: DummyQuery = DummyQuery("")
+}
+
+class AbstractQueryTest extends FlatSpec with Matchers {
+
+  it should "create an empty CQL query using the empty method on the companion object" in {
+    DummyQuery.empty.queryString shouldEqual ""
+  }
+}
